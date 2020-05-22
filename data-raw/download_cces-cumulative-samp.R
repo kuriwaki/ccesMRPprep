ab5b6e5ad7ad378b6ec1df11809e9d0ce08112d8
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-05-22 13:59:22 -0400

    sample ccc data

---
+++ data-raw/download_cces-cumulative-samp.R
@@ -0,0 +1,20 @@
+source("R/my_API_tokens.R")
+
+library(tidyverse)
+library(dataverse)
+library(ccesMRPprep)
+
+
+
+ccc <- get_cces_dv("cumulative")
+
+set.seed(02138)
+ccc_samp <- ccc %>%
+  sample_n(1000) %>%
+  arrange(year, case_id) %>%
+  select(year, case_id, state:cd, zipcode, county_fips,
+         age, race, hispanic, educ, faminc, marstat, newsint,
+         vv_turnout_gvm, voted_pres_16, economy_retro)
+
+
+usethis::use_data(ccc_samp, overwrite = TRUE)
