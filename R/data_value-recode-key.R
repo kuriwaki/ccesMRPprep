6b6a620c53e555cc50f02533511710ec46e4a842
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-05-22 13:36:06 -0400

    better doc

--- R/data_value-recode-key.R
+++ R/data_value-recode-key.R
@@ -1,17 +1,29 @@
 #' CCES-ACS variable recode keys
 #'
-#' These value-value tables are useful for recoding the values of
-#' from one dataset (CCES) so that they can be merged immediately with another (ACS).
+#' @details These value-value tables are useful for recoding the values of
+#' from one dataset (CCES) so that they can be merged immediately with another
+#' (ACS). These get used internally in \link{ccc_std_demographics}, but they are
+#' available as built in datasets
 #'
 #'
 #' @format A tibble with one row per recoding value
 #'  \describe{
-#'  \item{race}{An labelled integer of class haven::labelled. Most compact form
-#'  of all the data.}
-#'  \item{race_chr, gender_chr, educ_chr}{Labels, in characters}
-#'  \item{race_cces, edu_cces}{Corresponding labelled in the CCES cumulative common content}
+#'  \item{race, educ, gender}{An labelled integer of class haven::labelled.
+#'     Most compact form of both sources and the values both will get recoded
+#'     to in MRP.}
+#'  \item{race_chr, gender_chr, educ_chr}{Labels for the first column, in characters}
+#'  \item{race_cces_chr, edu_cces_chr}{Corresponding labelled in the CCES cumulative
+#'        common content}
 #'  \item{race_acs}{Corresponding character in the ACS data via the tidycensus package}
 #'  }
+#'
+#'
+#' @examples
+#'  library(ccesMRPprep)
+#'  educ_key
+#'  race_key
+#'
+#'
 #'
 "race_key"
 
