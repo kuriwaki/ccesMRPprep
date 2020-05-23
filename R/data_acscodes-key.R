a5c0f3f06f3db03f2b25ff06e6763f5ed67f4e61
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-05-23 16:19:03 -0400

    port more of old code to clean up ACS and match for CCES

--- R/data_acscodes-key.R
+++ R/data_acscodes-key.R
@@ -8,7 +8,7 @@
 #' these will be collapsed for MRP to form a common denominator with the CCES; see
 #' \link{educ_key}
 #'
-#' @seealso \link{acscodes_age_sex_rac}
+#' @seealso \link{acscodes_age_sex_race}
 #'
 #' @examples
 #' head(acscodes_age_sex_educ)
@@ -25,8 +25,40 @@
 #' these will be collapsed for MRP to form a common denominator with the CCES; see
 #' \link{race_key}
 #'
-#' @seealso \link{acscodes_age_sex_edu}
+#' @seealso \link{acscodes_age_sex_educ}
 #'
 #' @examples
 #' head(acscodes_age_sex_race)
 "acscodes_age_sex_race"
+
+
+#' ACS code lookup table
+#'
+#' A tidy dataframe where each row is a ACS code. This is useful internal
+#' data to give meaning to variable codes e.g. \link{acscodes_age_sex_educ}
+#'
+#' @format A data frame where each row represents a column.
+#' \describe{
+#'  \item{variable}{the ACS code (2018)}
+#'  \item{gender}{A labelled variable for gender. 1 is Male, 2 is Female. Use
+#'    the \code{labelled} or \code{haven} package to see labels.}
+#'  \item{age}{A labelled variable specifying which age biin the variable specifies}
+#'  \item{race}{A labelled variable specifying which education bin the variable specifies}
+#'  \item{educ}{A labelled variable specifying which race binthe variable specifies}
+#'  }
+#'
+#' @details The 5-yr ACS at 2018 is used,
+#' although codes should be fairly consistent across time. IF a demographic variable is NA,
+#' that means the variable collapses over the levels of that variable. In other
+#' wods, NA here can be thought of as meaning "all".
+#'
+#' @source Modifications around tidycensus::load_variables
+#'
+#' @importFrom labelled labelled
+#' @importFrom tibble tibble
+#'
+#'
+#' @examples
+#'  head(acscodes_df)
+#'
+"acscodes_df"
--- data-raw/specify_ACS-codes.R
+++ data-raw/specify_ACS-codes.R
@@ -1,78 +1,89 @@
+library(ccesMRPprep)
+library(tidycensus)
+library(tidyverse)
 
-# method to get these codes
-if (FALSE) {
-  library(tidycensus)
+# categories in ACS
+ages  <- c("18 to 24 years",
+           "25 to 34 years",
+           "35 to 44 years",
+           "45 to 64 years",
+           "65 years and over",
+           "18 and 19 years",
+           "20 to 24 years",
+           "25 to 29 years",
+           "30 to 34 years",
+           "35 to 44 years",
+           "45 to 54 years",
+           "55 to 64 years",
+           "65 to 74 years",
+           "75 to 84 years",
+           "85 years and over")
+education <- c("Less than 9th grade",
+               "9th to 12th grade,? no diploma",
+               "High school graduate \\(includes equivalency\\)",
+               "Some college,? no degree",
+               "Some college or associate's degree",
+               "Associate's degree",
+               "Bachelor's degree",
+               "Graduate or professional degree")
+races <- c("White alone, not Hispanic or Latino",
+           "Hispanic or Latino",
+           "Black or African American alone",
+           "American Indian and Alaska Native alone",
+           "Asian alone",
+           "Native Hawaiian and Other Pacific Islander alone",
+           "Some other race alone",
+           "Two or more races" #,
+           # "Two or more races!!Two races including Some other race",
+           # "Two or more races!!Two races excluding Some other race, and three or more races"
+)
 
-  # categories in ACS
-  ages  <- c("18 to 24 years",
-             "25 to 34 years",
-             "35 to 44 years",
-             "45 to 64 years",
-             "65 years and over",
-             "18 and 19 years",
-             "20 to 24 years",
-             "25 to 29 years",
-             "30 to 34 years",
-             "35 to 44 years",
-             "45 to 54 years",
-             "55 to 64 years",
-             "65 to 74 years",
-             "75 to 84 years",
-             "85 years and over")
-  education <- c("Less than 9th grade",
-                 "9th to 12th grade,? no diploma",
-                 "High school graduate \\(includes equivalency\\)",
-                 "Some college,? no degree",
-                 "Some college or associate's degree",
-                 "Associate's degree",
-                 "Bachelor's degree",
-                 "Graduate or professional degree")
-  races <- c("White alone, not Hispanic or Latino",
-             "Hispanic or Latino",
-             "Black or African American alone",
-             "American Indian and Alaska Native alone",
-             "Asian alone",
-             "Native Hawaiian and Other Pacific Islander alone",
-             "Some other race alone",
-             "Two or more races" #,
-             # "Two or more races!!Two races including Some other race",
-             # "Two or more races!!Two races excluding Some other race, and three or more races"
-  )
-
-  ages_regex  <- as.character(glue("({str_c(ages, collapse = '|')})"))
-  edu_regex   <- as.character(glue("({str_c(education, collapse = '|')})"))
-  races_regex <- as.character(glue("({str_c(races, collapse = '|')})"))
-
-
-  # get vars ----
-  vars_raw <- load_variables(2017, "acs5")
-
-  # format these and recode
-  vars <- vars_raw %>%
-    mutate(variable = name) %>%
-    separate(name, sep = "_", into = c("table", "num")) %>%
-    select(variable, table, concept, num, label, everything()) %>%
-    filter(str_detect(label, "Total")) %>%
-    mutate(label = str_remove(label, "Estimate!!Total")) %>%
-    mutate(gender = str_extract(label, "(Male|Female)"),
-           age = str_extract(label, ages_regex),
-           educ = str_extract(label, edu_regex),
-           race = coalesce(str_extract(label, regex(races_regex, ignore_case = TRUE)),
-                           str_extract(concept, regex(races_regex, ignore_case = TRUE))))
-
-  # partition vars to pull
-  acscodes_age_sex_educ <- vars %>%
-    filter(!is.na(gender), !is.na(age), !is.na(educ)) %>%
-    filter(str_detect(table, "^B")) %>%
-    pull(variable)
-
-  acscodes_age_sex_race <- vars %>%
-    filter(str_detect(table, "B01001[B-I]")) %>%
-    filter(!is.na(gender), !is.na(age), !is.na(race)) %>%
-    pull(variable)
-}
+ages_regex  <- as.character(glue("({str_c(ages, collapse = '|')})"))
+edu_regex   <- as.character(glue("({str_c(education, collapse = '|')})"))
+races_regex <- as.character(glue("({str_c(races, collapse = '|')})"))
 
 
+# get vars ----
+vars_raw <- tidycensus::load_variables(2018, "acs5")
+
+# format these and recode
+# to strings ----
+vars <- vars_raw %>%
+  mutate(variable = name) %>%
+  separate(name, sep = "_", into = c("table", "num")) %>%
+  select(variable, table, concept, num, label, everything()) %>%
+  filter(str_detect(label, "Total")) %>%
+  mutate(label = str_remove(label, "Estimate!!Total")) %>%
+  mutate(gender = str_extract(label, "(Male|Female)"),
+         age = str_extract(label, ages_regex),
+         educ = str_extract(label, edu_regex),
+         race = coalesce(str_extract(label, regex(races_regex, ignore_case = TRUE)),
+                         str_extract(concept, regex(races_regex, ignore_case = TRUE))))
+
+# partition vars to pull
+acscodes_age_sex_educ <- vars %>%
+  filter(!is.na(gender), !is.na(age), !is.na(educ)) %>%
+  filter(str_detect(table, "^B")) %>%
+  pull(variable)
+
+acscodes_age_sex_race <- vars %>%
+  filter(str_detect(table, "B01001[B-I]")) %>%
+  filter(!is.na(gender), !is.na(age), !is.na(race)) %>%
+  pull(variable)
+
+# to labelled
+acscodes_df <- vars %>%
+  mutate(race = str_to_upper(race)) %>%
+  rename(gender_chr = gender, age_chr = age, educ_chr = educ, race_acs = race) %>%
+  left_join(gender_key, by = "gender_chr") %>%
+  left_join(age5_key, by = "age_chr") %>% # age 10 if using race interactions consider binding while keeping the label
+  left_join(educ_key2, by = "educ_chr") %>%
+  left_join(filter(race_key, !is.na(race_acs)), by = "race_acs") %>%
+  select(variable, gender, age, educ, race)
+
+
+# literal writeup -------
+# (see above for more duplicate, source script)
 acscodes_age_sex_educ <- c(
   "B15001_004",
   "B15001_005",
@@ -308,6 +319,8 @@ acscodes_age_sex_race <- c(
   "B01001I_031")
 
 
+# Write ----
 usethis::use_data(acscodes_age_sex_educ, overwrite = TRUE)
 usethis::use_data(acscodes_age_sex_race, overwrite = TRUE)
+usethis::use_data(acscodes_df, overwrite = TRUE)
 
