48cdc2a3de8ada41d149906245253bf4fdf7f448
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-05-24 09:29:24 -0400

    rename, add year argument

--- R/get_dataverse.R
+++ R/get_dataverse.R
@@ -14,6 +14,10 @@
 #'
 #'
 #' @param name The name of the dataset as defined in \code{data(cces_dv_ids)}.
+#' @param year_subset The year (or years, a vector) to subset too. If `name` is a year specific dataset, this
+#' argument is redundant, but if `name == "cumulative"`, then the output
+#' will be the cumulative dataset subsetted to that year. This is useful when
+#' using the cumulative dataset for its harmonized variables.
 #' @param dataverse_paths A dataframe where one row represents metadata for one
 #' CCES dataset. Built-in data \code{cces_dv_ids} is used as a default and should
 #' not be changed.
@@ -28,13 +32,15 @@
 #' @examples
 #'
 #' \dontrun{
-#'  ccc <- get_cces_dv("cumulative")
+#'  ccc <- get_cces_dv("cumulative", year = 2018)
 #'  }
 #'
 #' @export
 #'
 #'
-get_cces_dv <- function(name = cces_dv_ids$cces_name, dataverse_paths = cces_dv_ids) {
+get_cces_dv <- function(name = "cumulative",
+                        year_subset = 2006:2019,
+                        dataverse_paths = cces_dv_ids) {
 
   y_info <- filter(dataverse_paths, cces_name == as.character(name))
   filetype <- str_extract(y_info$filename, "\\.[A-z]+$")
@@ -56,5 +62,8 @@ get_cces_dv <- function(name = cces_dv_ids$cces_name, dataverse_paths = cces_dv_
   if (filetype == ".Rds")
     cces_raw <- readr::read_rds(tmp)
 
+  if (name == "cumulative") {
+    cces_raw <- filter(cces_raw, year %in% year_subset)
+  }
   return(cces_raw)
 }
