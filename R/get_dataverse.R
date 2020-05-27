4ca5d4396cb31946fa0b5f892f1eae371d13b3f5
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-05-27 09:38:15 -0400

    dataverse does not need api key to read data

--- R/get_dataverse.R
+++ R/get_dataverse.R
@@ -1,17 +1,14 @@
 #' Download a specific CCES dataset from dataverse
 #'
-#' You must download the development version of https://github.com/IQSS/dataverse-client-r
-#'  and get your own dataverse API Key. The current dataverse package downloads
+#' The current dataverse package downloads
 #'  the raw data, so this function writes the raw binary into a tempfile and
 #'  loads it into a tibble with the appropriate file data type.
-#'
-#'  To get a user key, you need to have your own dataverse account. Go to your account
-#'  online and look for "API Token". Generate a API Token. You can then save your
-#'  token by following \code{dataverse::get_user_key}, or directly saving
-#'  \code{Sys.setenv("DATAVERSE_KEY" = "YOUR KEY HERE")}
-#'  \code{Sys.setenv("DATAVERSE_SERVER" = "dataverse.harvard.edu")}
+#' You must download the development version of
+#' [IQSS/dataverse-client-r](https://github.com/IQSS/dataverse-client-r).
 #'
 #'
+#' @details
+#'  To write to dataverse, you will need to create your own dataverse API Token.
 #'
 #' @param name The name of the dataset as defined in \code{data(cces_dv_ids)}.
 #' @param year_subset The year (or years, a vector) to subset too. If `name` is a year specific dataset, this
@@ -44,15 +41,19 @@ get_cces_dv <- function(name = "cumulative",
 
   y_info <- filter(dataverse_paths, cces_name == as.character(name))
   filetype <- str_extract(y_info$filename, "\\.[A-z]+$")
+  svr <- y_info$server
 
-  if (filetype == ".tab")
-    filetype <- ".dta"
 
-  cces_dv <- get_file(glue("{y_info$filename}"), glue("doi:{y_info$doi}"))
+  cces_dv <- get_file(file = glue("{y_info$filename}"),
+                      dataset = glue("doi:{y_info$doi}"),
+                      server = svr)
   tmp <- tempfile(fileext = filetype)
   writeBin(cces_dv, tmp)
 
-  # read select columns ---
+  # read data  into df ---
+  if (filetype == ".tab")
+    filetype <- ".dta"
+
   if (filetype == ".dta")
     cces_raw <- haven::read_dta(tmp)
 
--- data-raw/create_dataverse-ids.R
+++ data-raw/create_dataverse-ids.R
@@ -1,21 +1,21 @@
 library(tibble)
 
 cces_dv_ids <- tribble(
-  ~cces_name, ~year, ~caseid_var, ~doi, ~filename,
-  "cumulative", NA, "case_id", "10.7910/DVN/II2DB6", "cumulative_2006_2018.Rds",
-  "2006", 2006, "v1000",  "10.7910/DVN/Q8HC9N", "cces_2006_common.tab",
-  "2007", 2007, "caseid", "10.7910/DVN/OOXTJ5", "CCES07_OUTPUT.sav",
-  "2008", 2008, "V100",   "10.7910/DVN/YUYIVB", "cces_2008_common.tab",
-  "2009", 2009, "v100",   "10.7910/DVN/KKM9UK", "cces09_cmn_output_2.dta",
-  "2010", 2010, "V100",   "10.7910/DVN/VKKRWA", "cces_2010_common_validated.dta",
-  "2011", 2011, "V100",   "10.7910/DVN/GLRXZ0", "CCES11_Common_OUTPUT.tab",
-  "2012", 2012, "V101",   "10.7910/DVN/HQEVPK", "CCES12_Common_VV.tab",
-  "2013", 2013, "caseid", "10.7910/DVN/KPP85M", "Common Content Data.tab",
-  "2014", 2014, "V101",   "10.7910/DVN/XFXJVY", "CCES14_Common_Content_Validated.tab",
-  "2015", 2015, "V101",   "10.7910/DVN/SWMWX8", "CCES15_Common_OUTPUT_Jan2016.tab",
-  "2016", 2016, "V101",   "10.7910/DVN/GDF6Z0", "CCES16_Common_OUTPUT_Feb2018_VV.tab",
-  "2017", 2017, "V101",   "10.7910/DVN/3STEZY", "Common Content Data.tab",
-  "2018", 2018, "caseid", "10.7910/DVN/ZSBZ7K", "cces18_common_vv.dta",
+  ~cces_name, ~year, ~doi, ~filename,  ~caseid_var, ~server,
+  "cumulative", NA,   "10.7910/DVN/II2DB6", "cumulative_2006_2018.Rds", "case_id", "dataverse.harvard.edu",
+  "2006",       2006, "10.7910/DVN/Q8HC9N", "cces_2006_common.tab", "v1000",   "dataverse.harvard.edu",
+  "2007",       2007, "10.7910/DVN/OOXTJ5", "CCES07_OUTPUT.sav", "caseid",  "dataverse.harvard.edu",
+  "2008",       2008, "10.7910/DVN/YUYIVB", "cces_2008_common.tab", "V100",    "dataverse.harvard.edu",
+  "2009",       2009, "10.7910/DVN/KKM9UK", "cces09_cmn_output_2.dta", "v100",    "dataverse.harvard.edu",
+  "2010",       2010, "10.7910/DVN/VKKRWA", "cces_2010_common_validated.dta", "V100",    "dataverse.harvard.edu",
+  "2011",       2011, "10.7910/DVN/GLRXZ0", "CCES11_Common_OUTPUT.tab", "V100",    "dataverse.harvard.edu",
+  "2012",       2012, "10.7910/DVN/HQEVPK", "CCES12_Common_VV.tab", "V101",    "dataverse.harvard.edu",
+  "2013",       2013, "10.7910/DVN/KPP85M", "Common Content Data.tab", "caseid",  "dataverse.harvard.edu",
+  "2014",       2014, "10.7910/DVN/XFXJVY", "CCES14_Common_Content_Validated.tab", "V101",    "dataverse.harvard.edu",
+  "2015",       2015, "10.7910/DVN/SWMWX8", "CCES15_Common_OUTPUT_Jan2016.tab", "V101",    "dataverse.harvard.edu",
+  "2016",       2016, "10.7910/DVN/GDF6Z0", "CCES16_Common_OUTPUT_Feb2018_VV.tab", "V101",    "dataverse.harvard.edu",
+  "2017",       2017, "10.7910/DVN/3STEZY", "Common Content Data.tab", "V101",    "dataverse.harvard.edu",
+  "2018",       2018, "10.7910/DVN/ZSBZ7K", "cces18_common_vv.dta", "caseid",  "dataverse.harvard.edu",
 )
 
 
