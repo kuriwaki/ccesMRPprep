da2b2cf7ff56586d7dc6f8e8a8c4fd45d52e446c
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-05-29 13:58:57 -0400

    sample question data, deriving from https://github.com/kuriwaki/CCES_district-opinion/issues/11

---
+++ R/datadoc_question-metadata.R
@@ -0,0 +1,24 @@
+#' Question metadata (sample)
+#'
+#'
+#' @description A tibble with `r nrow(questions_samp)` rows, each row corresponding
+#' to a variable (column) in a particular CCES dataset. It contains metadata about
+#' the question in columns.
+#' \description{
+#' \item{q_ID}{A unique identifier for the question. This is roughly a concatenation
+#'  of \code{cces_year} and \code{q_code}, with some standardization.}
+#' \item{q_label}{A short descriptive label of the question, based on a reading of the
+#'  actual question wording.}
+#' \item{cces_data}{The dataset the variable column comes from. Because the CCES is fielded
+#' each year, this is often a year, with the exception of \code{"cumulative"}}
+#' \item{q_code}{A string of the variable as it appears in the dataset.}
+#' \item{response_type}{A classification of the response option. Currently it consists
+#'  of \code{"yesno"}, \code{"categorical"} (discrete but not ordered), \code{"ordinal"}
+#'  (discrete and ordered). All of these presume a discrete response.}
+#' }
+#' @importFrom tibble tibble
+#'
+#' @examples
+#'  questions_samp
+#'
+"questions_samp"
---
+++ data-raw/question-metadata-samp.R
@@ -0,0 +1,35 @@
+questions_samp <- tibble::tribble(
+  ~q_ID,                       ~q_label,  ~cces_data,      ~q_code, ~response_type,
+  "CC06_3075",     "Cut Capital Gains Tax",  "2006",      "v3075", "yesno",
+  "CC06_3069",        "Immigration Reform",  "2006",      "v3069", "yesno",
+  "CC07_34",                  "SCHIP 2007",  "2007",       "CC34", "yesno",
+  "CC08_316E",                "SCHIP 2007",  "2008",     "CC316e", "yesno",
+  "CC09_59F",                      "PPACA",  "2009",   "cc09_59f", "yesno",
+  "CC10_332D",                     "PPACA",  "2010",     "CC332D", "yesno",
+  "CC11_341D",                     "PPACA",  "2011",     "CC341D", "yesno",
+  "CC12_332G",           "Repeal ACA 2012",  "2012",     "CC332G", "yesno",
+  "CC12_332I",                     "PPACA",  "2012",     "CC332I", "yesno",
+  "CC12_332D",           "Tax Relief 2012",  "2012",     "CC332D", "yesno",
+  "CC12_332C",      "Middle Class Tax Cut",  "2012",     "CC332C", "yesno",
+  "CC13_332C",           "Repeal ACA 2013",  "2013",     "CC332C", "yesno",
+  "CC14_325_5",  "Raise Debt Ceiling 2014",  "2014", "CC14_325_5", "yesno",
+  "CC14_325_3",           "Extend Tax Cut",  "2014", "CC14_325_3", "yesno",
+  "CC15_327A",           "Repeal ACA 2015",  "2015",  "CC15_327A", "yesno",
+  "CC16_351I",           "Repeal ACA 2015",  "2016",  "CC16_351I", "yesno",
+  "CC17_340A",           "Repeal ACA 2017",  "2017",  "CC17_340A", "yesno",
+  "CC17_340G", "Withold Sanctuary Funding",  "2017",  "CC17_340G", "yesno",
+  "CC18_326",           "Tax Cut Jobs Act",  "2018",   "CC18_326", "yesno",
+  "CC18_322C", "Withold Sanctuary Funding",  "2018",  "CC18_322c", "yesno",
+  "CC18_pid3", "Partisan Identification",     "cumulative",  "pid3", "categorical",
+  "CC18_pid7", "Partisan Identification",     "cumulative",  "pid7", "categorical",
+  "CCC_turnout_g", "Turnout in the General Election", "cumulative", "vv_turnout_gvm", "categorical",
+  "CCC_econ_retro",  "Retrospective Economy", "cumulative", "economy_retro", "ordinal",
+  "CCC_voted_sen", "Party vote for US Senate", "cumulative", "voted_sen_party", "categorical",
+  "CCC_voted_rep", "Party vote for US Representative", "cumulative", "voted_rep_party", "categorical",
+  "CCC_voted_gov", "Party vote for Governor", "cumulative", "voted_gov_party", "categorical",
+  "CC16_422F", "Racism is isolated", "2016", "CC16_422f", "ordinal",
+)
+
+
+
+usethis::use_data(questions_samp, overwrite = TRUE)
