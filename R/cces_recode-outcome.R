842ee8783d223e475a28cc154e4b46cb144ea9d0
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-06-14 20:50:03 -0400

    fix ambiguity with "don't know" -- if they get coded as 0s, that unerestimates the support

--- R/cces_recode-outcome.R
+++ R/cces_recode-outcome.R
@@ -5,10 +5,12 @@
 #' but also "Support" and "For"
 #'
 #' @param vec A vector of CCES response values, in character
+#' @param DK_to_NA Whether don't knows should be treated as NA. Defaults to TRUE.
 #'
 #' @return A vector of integers
 #'
-#' @importFrom stringr str_detect
+#' @importFrom stringr str_detect str_replace_all
+#' @importFrom dplyr recode
 #'
 #' @examples
 #'  yesno_to_binary(
@@ -20,10 +22,22 @@
 #'     "Somewhat oppose",
 #'     "For this bill",
 #'     "Against this bill",
+#'     "Not sure",
+#'     "Don't Know",
 #'     NA_character_)
 #' )
 #' @export
-yesno_to_binary <- function(vec) {
-  as.integer(vec %in% c("For", "for", "Support", "support",  "yes", "Yes") |
-               str_detect(vec, "For this bill"))
+yesno_to_binary <- function(vec, DK_to_NA = TRUE) {
+
+  # Yes
+  vec_char <- vec %>%
+    replace(str_detect(., regex("^Against", ignore_case = TRUE)), "Against") %>%
+    replace(str_detect(., regex("^For", ignore_case = TRUE)), "For") %>%
+    str_replace_all(regex("somewhat\\s+", ignore_case = TRUE), "") %>%
+    replace(. %in% c("Against", "Oppose", "oppose", "against", "no", "No"), "N") %>%
+    replace(. %in% c("For", "for", "Support", "support",  "yes", "Yes"), "Y") %>%
+    replace(. %in% c("Don't Know", "Don't know", "dk", "don't know",  "Not Sure", "not sure", "not sure "), "DK") %>%
+    replace(. %in% c("Not Asked", "Skipped", "skipped"), NA)
+
+  recode(vec_char, Y = 1, N = 0, DK = ifelse(DK_to_NA, NA_real_, 0), .default = NA_real_)
 }
