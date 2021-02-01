61783c87ec0dada9ad2ed5f4c3de550799a6e239
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-02-01 14:58:57 -0500

    Add female

--- data-raw/download_NY-2018.R
+++ data-raw/download_NY-2018.R
@@ -14,8 +14,9 @@ cc18_NY_full <- get_cces_dataverse("cumulative", year_subset = 2018) %>%
   filter(st == "NY")
 
 cc18_NY <- cc18_NY_full %>%
+  mutate(female = as.numeric(gender == 2)) %>%
   select(year, case_id, state:cd, county_fips,
-         gender, age, race, hispanic, educ, faminc, marstat, newsint,
+         gender, female, age, race, hispanic, educ, faminc, marstat, newsint,
          matches("^pid"),
          vv_turnout_gvm, voted_pres_16,
          matches("(intent|voted)_[.+]_party")) %>%
@@ -26,20 +27,3 @@ usethis::use_data(cc18_NY, overwrite = TRUE)
 usethis::use_data(acs_NY, overwrite = TRUE)
 usethis::use_data(elec_NY, overwrite = TRUE)
 
-library(Formula)
-library(emlogit)
-
-imputed_acs <- impute_cell(pid3 ~ race + age, cc18_NY, acs_NY, area_var = "cd")
-
-imputed_acs %>%
-  count(cd, pid3, wt = count) %>%
-  group_by(cd) %>%
-  summarize(twoparty_gop = sum(n*(pid3 == "Republican")) / sum(n*(pid3 %in% c("Republican", "Democrat")))) %>%
-  left_join(elec_NY) %>%
-  ggplot(aes(x = pct_trump, twoparty_gop)) +
-  geom_point() +
-  coord_equal() +
-  geom_abline(linetype = "dashed") +
-  geom_label(aes(label = largest_place))
-
-library(ggplot2)
