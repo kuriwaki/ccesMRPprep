875291b8fca3dee20588491ba1bb15531d0f200a
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-02-01 23:13:25 -0500

    Rename to synth_mlogit

--- R/synth_additional-X.R
+++ R/synth_additional-X.R
@@ -1,18 +1,23 @@
-#' Impute joint distribution of categorical variables
+#' Model synthetic joint distribution of categorical variables
 #'
 #' Imputes the counts of a joint distribution of count variables for small areas
 #' based on microdata. In this setup, the population distribution table (`poptable`)
-#' has the joint distribution of `(A, X_1, ..., X_{K - 1})` categorical variables  where.
-#' `A` denotes a categorical small area and `X`s denote categorical covariates.
-#' Now, the survey data (`microdata`) has a sample joint distribution of
-#' `(X_1, .., X_{K - 1}, X_{K})` categorical variables but the sample size is too small
-#' for small areas. Therefore, the function models a multinomial outcome model roughly of the
-#' form `X_{K} ~ X_1 + ... X_{X_K}` and predicts onto `poptable` to estimate the
-#' joint distribution of `(A, X_1, .., X_{K})`
+#' has the joint distribution of `(A, X_{1}, ..., X_{K - 1})` categorical variables where
+#' `A` denotes a categorical small area, `X`s denote categorical covariates, and
+#' the missing covariate is `X_{K}`.
 #'
+#' Now, the survey data (`microdata`) has a sample joint distribution of
+#' `(X_{1}, .., X_{K - 1}, X_{K})` categorical variables but the sample size is too small
+#' for small areas. Therefore, the function models a multinomial outcome model roughly of the
+#' form `X_{K} ~ X_{1} + ... X_{K}` and predicts onto `poptable` to estimate the
+#' joint distribution of `(A, X_{1}, .., X_{K})`
+#'
+#' Currently, this function does not support post-stratifiation based on a known aggregate
+#' distribution -- that is, further adjusting the probabilities based on
+#' a known population distribution (see e.g. Leeman and Wasserfallen AJPS <https://doi.org/10.1111/ajps.12319>)
 #'
 #' @param formula A representation of the aggregate imputation or "outcome" model,
-#'  of the form `X_{K} ~ X_1 + ... X_{X_K}`
+#'  of the form `X_{K} ~ X_1 + ... X_{K - 1}`
 #' @param microdata The survey table that the multinomial model will be built off.
 #'  Must contain all variables in the LHS and RHS of `formula`.
 #' @param poptable The population table, collapsed in terms of counts. Must contain
@@ -21,6 +26,22 @@
 #' @param area_var A character vector of the area of interest.
 #' @param count_var A character variable that specifies which variable in `poptable`
 #'  indicates the count
+#'
+#'
+#' @returns A dataframe with a similar format as the `poptarget` table
+#'  but with rows expanded to serve as a joint distribution. In general,
+#'  if the variable of interest has `L` values, the final dataset will have
+#'  `L` times more rows than `poptarget`. The data will have additional variables:
+#'
+#'  * `n_aggregate`: The sum of counts known in the aggregate. i.e., the number of
+#'   trials the multinomial will consider. This is the sum of the original
+#'   `count` variables.
+#'  * The outcome variable of interest. For example if the LHS of the formula
+#'     was `party_id`, then there would be a column called `party_id` containing the
+#'     values of that variable in long form.
+#'  * `pr_pred`: The predicted probability of taking the value of the outcome. This
+#'   is the main output of the multinomial model.
+#'  * `count`: A new count variable. Simply the product of `n_aggregate` and `pr_pred`.
 #'
 #'
 #' @export
@@ -37,13 +58,15 @@
 #'  Yair Ghitza and Mark Steitz (2020). DEEP-MAPS Model of the Labor Force.
 #'    Working Paper. <https://github.com/Catalist-LLC/unemployment>
 #'
+#'
+#'
 #' @examples
 #'  library(dplyr)
 #'
 #'  # Impute the joint distribution of party ID with race, sex, and age, using
 #'  # survey data in NY.
 #'
-#'  imputed_acs <- impute_cells(pid3 ~ race + age + female,
+#'  imputed_acs <- synth_mlogit(pid3 ~ race + age + female,
 #'                              microdata = cc18_NY,
 #'                              poptable = acs_NY,
 #'                              area_var = "cd")
@@ -60,13 +83,13 @@
 #'\dontrun{
 #'   # another example -- imputing education
 #'   library(ccesMRPrun)
-#'   impute_cells(educ ~ age + female,
+#'   synth_mlogit(educ ~ age + female,
 #'               microdata = cces_GA,
 #'               poptable = acs_GA,
 #'               area_var = "cd")
 #'}
 #'
-impute_cells <- function(formula,
+synth_mlogit <- function(formula,
                         microdata,
                         poptable,
                         area_var,
