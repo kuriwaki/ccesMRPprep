6c8e0b041b9d0f0a223b05ac83de8397aa3fcd66
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-04-19 01:01:29 -0400

    Update to 2020

--- data-raw/create_dataverse-ids.R
+++ data-raw/create_dataverse-ids.R
@@ -2,7 +2,7 @@ library(tibble)
 
 cces_dv_ids <- tribble(
   ~cces_name, ~year, ~doi, ~filename,  ~caseid_var, ~server,
-  "cumulative", NA,   "10.7910/DVN/II2DB6", "cumulative_2006-2019.dta", "case_id", "dataverse.harvard.edu",
+  "cumulative", NA,   "10.7910/DVN/II2DB6", "cumulative_2006-2020.dta", "case_id", "dataverse.harvard.edu",
   "2006",       2006, "10.7910/DVN/Q8HC9N", "cces_2006_common.tab", "v1000",   "dataverse.harvard.edu",
   "2007",       2007, "10.7910/DVN/OOXTJ5", "CCES07_OUTPUT.sav", "caseid",  "dataverse.harvard.edu",
   "2008",       2008, "10.7910/DVN/YUYIVB", "cces_2008_common.tab", "V100",    "dataverse.harvard.edu",
@@ -17,6 +17,7 @@ cces_dv_ids <- tribble(
   "2017",       2017, "10.7910/DVN/3STEZY", "Common Content Data.tab", "V101",    "dataverse.harvard.edu",
   "2018",       2018, "10.7910/DVN/ZSBZ7K", "cces18_common_vv.dta", "caseid",  "dataverse.harvard.edu",
   "2019",       2019, "10.7910/DVN/WOT7O8", "CCES19_Common_OUTPUT.tab", "caseid",  "dataverse.harvard.edu",
+  "2020",       2020, "10.7910/DVN/E9N6PH", "CCES20_Common_OUTPUT.dta", "caseid",  "dataverse.harvard.edu",
 )
 
 
