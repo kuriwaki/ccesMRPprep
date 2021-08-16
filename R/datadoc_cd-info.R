252e37288e8704ad62dd5c81e65093f78768c76a
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-08-16 18:13:13 -0400

    Add 2020 from CCES rep

--- R/datadoc_cd-info.R
+++ R/datadoc_cd-info.R
@@ -1,10 +1,10 @@
-#' Congressional District level information
+#' Congressional District level information by Daily Kos
 #'
 #'
 #' Some of the most consequential variables to include in MRP are at the
 #' district-level. We include one such data for congressional districts. All data
 #' is collected by Daily Kos. `cd_info_2018` is data on 2018 boundaries, `cd_info_2016`
-#' uses 2016 boundaries (but with place descriptions currently at 2016).
+#' uses 2016 boundaries and `cd_info_2020` uses 2020 (but with place descriptions currently at 2016).
 #'
 #' @format `cd_info_2018` is a dataframe with the `r nrow(cd_info_2018)` Congressional
 #'  Districts with 2018 boundaries, one row per cd.
@@ -20,7 +20,8 @@
 #'    \item{pct_trump, pct_romney, pct_mccain}{The two-party voteshare of Republican presidential candidates
 #'    in that district for the given year. E.g. the \code{pct_mccain} data when \code{cd_year == 2018}
 #'    represents the percent of the vote by McCain in 2008 for that district _under 2018 lines._
-#'    The Trump value is for 2016, not 2020.}
+#'    The Trump value is for 2016 for `cd_info_2018` and ``cd_info_2020` but not for 2020 where
+#'    we use Trump's 2020 vote against Biden and denote as `pct_trump16` the 2016 result.}
 #'    \item{dailykos_name}{The unique descriptive name for the district code in 2018 given by Daily Kos.
 #'    See Source for full citation.}
 #'    \item{largest_place}{The largest place in the district code in 2018 given by Daily Kos. Multiple districts may
@@ -46,6 +47,9 @@
 
 #' @rdname cd_info_2018
 "cd_info_2016"
+
+#' @rdname cd_info_2018
+"cd_info_2020"
 
 
 #' @rdname cd_info_2018
--- data-raw/create_dist-level-data.R
+++ data-raw/create_dist-level-data.R
@@ -8,10 +8,18 @@ library(fs)
 projdir <- "~/Dropbox/CCES_representation/data/output/"
 
 
+cd_info_2020 <- read_rds(path(projdir, "averages/by-cd-2020_info.Rds")) %>%
+  mutate(year = 2020) %>%
+  select(year, cd,
+         dailykos_name = descrip_18,
+         largest_place = place_18,
+         pct_trump, pct_trump16, pct_romney)
+
 cd_info_2018 <- read_rds(path(projdir, "averages/by-cd-2018_info.Rds")) %>%
   mutate(year = 2018) %>%
   select(year, cd,
-         dailykos_name = descrip, largest_place = place,
+         dailykos_name = descrip,
+         largest_place = place,
          pct_trump, pct_romney, pct_mccain)
 
 cd_info_2016 <- read_rds(path(projdir, "averages/by-cd-2016_info.Rds")) %>%
@@ -24,3 +32,4 @@ cd_info_2016 <- read_rds(path(projdir, "averages/by-cd-2016_info.Rds")) %>%
 
 usethis::use_data(cd_info_2016, overwrite = TRUE)
 usethis::use_data(cd_info_2018, overwrite = TRUE)
+usethis::use_data(cd_info_2020, overwrite = TRUE)
