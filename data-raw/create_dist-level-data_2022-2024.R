faa90b6b518468d7330e31c840f9ffd20eff5b69
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-09-17 09:46:10 -0400

    Pipe code style

--- data-raw/create_dist-level-data_2022-2024.R
+++ data-raw/create_dist-level-data_2022-2024.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(googlesheets4)
 
 # Authenticate with Google Sheets
-gs4_auth()
+# gs4_auth()
 
 # URLs for your Google Sheets
 url_2022 <- "https://docs.google.com/spreadsheets/d/1CKngqOp8fzU22JOlypoxNsxL6KSAH920Whc-rd7ebuM/edit?usp=sharing"
@@ -13,50 +13,50 @@ url_geo_118 <- "https://docs.google.com/spreadsheets/d/1weoLFu2U5lmxQNcB8pFItGHj
 # Read data from Google Sheets
 
 # 2022
-cd_info_2022 <- read_sheet(url_2022, sheet = 1) %>%
-  mutate(year = 2022) %>%
+cd_info_2022 <- read_sheet(url_2022, sheet = 1) |>
+  mutate(year = 2022) |>
   select(year, cd = District)
 
-voting_info_2022 <- read_sheet(url_2022, sheet = 2) %>%
+voting_info_2022 <- read_sheet(url_2022, sheet = 2) |>
   select(cd = District,
          presvotes_total = Total,
          pct_trump = 'Trump %')
 
-region_2022 <- read_sheet(url_geo_118, sheet = 1) %>%
+region_2022 <- read_sheet(url_geo_118, sheet = 1) |>
   select(cd = CD,
          dailykos_name = `Geographic Description`)
 
-largest_place_2022 <- read_sheet(url_geo_118, sheet = 2) %>%
+largest_place_2022 <- read_sheet(url_geo_118, sheet = 2) |>
   select(cd = CD,
          largest_place = `Largest place`)
 
 # 2024
-cd_info_2024 <- read_sheet(url_2024, sheet = 1) %>%
-  mutate(year = 2024) %>%
+cd_info_2024 <- read_sheet(url_2024, sheet = 1) |>
+  mutate(year = 2024) |>
   select(year, cd = District)
 
-voting_info_2024 <- read_sheet(url_2024, sheet = 2) %>%
+voting_info_2024 <- read_sheet(url_2024, sheet = 2) |>
   select(cd = District,
          presvotes_total = Total,
          pct_trump = 'Trump %')
 
-region_2024 <- read_sheet(url_geo_119, sheet = 1) %>%
+region_2024 <- read_sheet(url_geo_119, sheet = 1) |>
   select(cd = CD,
          dailykos_name = `Geographic Description`)
 
-largest_place_2024 <- read_sheet(url_geo_119, sheet = 2) %>%
+largest_place_2024 <- read_sheet(url_geo_119, sheet = 2) |>
   select(cd = CD,
          largest_place = `Largest place`)
 
 # Join geographic descriptions to the main datasets
-cd_info_2022 <- cd_info_2022 %>%
-  left_join(region_2022, by = "cd") %>%
-  left_join(largest_place_2022, by = "cd") %>%
+cd_info_2022 <- cd_info_2022 |>
+  left_join(region_2022, by = "cd") |>
+  left_join(largest_place_2022, by = "cd") |>
   left_join(voting_info_2022, by = "cd")
 
-cd_info_2024 <- cd_info_2024 %>%
-  left_join(region_2024, by = "cd") %>%
-  left_join(largest_place_2024, by = "cd") %>%
+cd_info_2024 <- cd_info_2024 |>
+  left_join(region_2024, by = "cd") |>
+  left_join(largest_place_2024, by = "cd") |>
   left_join(voting_info_2024, by = "cd")
 
 # Save the data
