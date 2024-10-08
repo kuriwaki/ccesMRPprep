5d0a055828492b35cd57b6753e7ddcdb38bb9d8c
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-10-08 10:44:22 -0400

    Add denominator to 2018 - 2016 data

--- data-raw/create_dist-level-data_2016-2020.R
+++ data-raw/create_dist-level-data_2016-2020.R
@@ -7,7 +7,7 @@ library(fs)
 
 
 projdir <- "~/Dropbox/CCES_representation/data/output/"
-
+url_2018 <- "https://docs.google.com/spreadsheets/d/1zLNAuRqPauss00HDz4XbTH2HqsCzMe0pR8QmD1K8jk8"
 
 cd_info_2020 <- read_rds(path(projdir, "averages/by-cd-2020_info.Rds")) %>%
   mutate(year = 2020) %>%
@@ -17,15 +17,26 @@ cd_info_2020 <- read_rds(path(projdir, "averages/by-cd-2020_info.Rds")) %>%
          presvotes_total = pres_all_total,
          pct_trump, pct_trump16, pct_romney)
 
-cd_info_2018 <- read_rds(path(projdir, "averages/by-cd-2018_info.Rds")) %>%
+# 2018 ----
+cd_2018_shares <- read_rds(path(projdir, "averages/by-cd-2018_info.Rds")) %>%
   mutate(year = 2018) %>%
   select(year, cd,
          dailykos_name = descrip,
          largest_place = place,
          pct_trump, pct_romney, pct_mccain)
 
+# denominator
+votes_2018 <-
+  read_sheet(url_2018, sheet = 2, skip = 1) |>
+  janitor::clean_names() |>
+  transmute(cd = district, presvotes_total = clinton + trump_12) |>
+  mutate(cd = str_replace(cd, "-AL", "-01"))
 
+cd_info_2018 <- cd_2018_shares |>
+  left_join(votes_2018, by = "cd") |>
+  relocate(presvotes_total, .after = pct_trump)
 
+# 2016 ---
 cd_info_2016 <- read_rds(path(projdir, "averages/by-cd-2016_info.Rds")) %>%
   mutate(year = 2016) %>%
   select(year, cd,
