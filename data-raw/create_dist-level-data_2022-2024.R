d4118bdcaa226b73b18b17f92b1df950091da329
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2025-09-24 15:37:04 -0400

    2024 variable edits

--- data-raw/create_dist-level-data_2022-2024.R
+++ data-raw/create_dist-level-data_2022-2024.R
@@ -8,8 +8,8 @@ library(janitor)
 # URLs for your Google Sheets
 url_2022 <- "https://docs.google.com/spreadsheets/d/1CKngqOp8fzU22JOlypoxNsxL6KSAH920Whc-rd7ebuM/edit?usp=sharing"
 url_2024 <- "https://docs.google.com/spreadsheets/d/1ng1i_Dm_RMDnEvauH44pgE6JCUsapcuu8F2pCfeLWFo/edit?gid=1491069057#gid=1491069057"
-url_geo_119 <- "https://docs.google.com/spreadsheets/d/12YaBonkqHAjkXhzyKlH2-1t-smZ6J5j76RCBSJEwQHo/edit?usp=sharing"
 url_geo_118 <- "https://docs.google.com/spreadsheets/d/1weoLFu2U5lmxQNcB8pFItGHj1Lb_M2E9Oi48sI4w1vY/edit?usp=sharing"
+url_geo_119 <- "https://docs.google.com/spreadsheets/d/12YaBonkqHAjkXhzyKlH2-1t-smZ6J5j76RCBSJEwQHo/edit?usp=sharing"
 
 # Read data from Google Sheets
 
@@ -25,40 +25,37 @@ voting_info_2022 <- read_sheet(url_2022, sheet = 2) |>
             presvotes_total = Total
   )
 
-region_2022 <- read_sheet(url_geo_118, sheet = 1) |>
+region_2022 <- read_sheet(url_geo_118, sheet = "Descriptive name") |>
   select(cd = CD,
          dailykos_name = `Geographic Description`)
 
-largest_place_2022 <- read_sheet(url_geo_118, sheet = 2) |>
+largest_place_2022 <- read_sheet(url_geo_118, sheet = "By places") |>
   select(cd = CD,
          largest_place = `Largest place`)
 
 # 2024
 # clean sheet
-sheet_2024 <- read_sheet(url_2024, sheet = 2) |>
+sheet_2024 <- read_sheet(url_2024, sheet = "Vote totals") |>
   select(1:9) |>
-  select(-2, -3) |>
   row_to_names(row_number = 2, remove_row = TRUE) |>
-  rename(cd = 1)
+  select(cd = 1, Harris, Trump, Total)
 
 cd_names_2024 <- sheet_2024 |>
-  mutate(year = 2024) |>
-  select(year, cd)
+  transmute(year = 2024, cd)
 
 voting_info_2024 <- sheet_2024 |>
-  select(1:4) |>
   mutate(across(where(is.list) & !matches("^cd$"),
                 ~ map_dbl(.x, ~ as.numeric(.x[1])))) |>
   transmute(cd,
-            pct_trump24 = Trump / (Harris + Trump),
-            presvotes_DR_24 = Harris + Trump,
-            presvotes_total_24 = Total)
+            pct_trump = Trump / (Harris + Trump),
+            presvotes_DR = Harris + Trump,
+            presvotes_total = Total)
 
-region_2024 <- read_sheet(url_geo_119, sheet = 1) |>
+region_2024 <- read_sheet(url_geo_119, sheet = "Descriptive name") |>
   select(cd = CD,
          dailykos_name = `Geographic Description`)
 
-largest_place_2024 <- read_sheet(url_geo_119, sheet = 2) |>
+largest_place_2024 <- read_sheet(url_geo_119, sheet = "By places") |>
   select(cd = CD,
          largest_place = `Largest place`)
 
